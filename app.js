3652ccdb6b90b2ff8b6c4586da065aaa8120a491
annagazzi-cell <anna.gazzi@escola.pr.gov.br>
2026-04-28 11:05:58 -0300

    V.frases

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 
-let frases=["F1",
-     "F2",
-     "F3"];
+let frases=["O SENHOR É MEU PASTOR; NADA ME FALTARÁ.    SALMO 23:1 🌿🐑💖",
+     "REGOZIJAI-VOS NO SENHOR, VÓS, OS JUSTOS; AOS PIEDOSOS É BELO O CÂNTICO.SALMO 33:1 🎶💫😇",
+     "FOSTE A MINHA AJUDA, E NA SOMBRA DAS TUAS ASAS ME ALEGRAREI.SALMO 63:7 🕊️🌸😊",
+    "A TERRA ESTÁ CHEIA DA BONDADE DO SENHOR.    SALMO 33:5 🌍💚🌸"];
 
 function geradorMensagem(){
     let indiceGerado= Math.floor(Math.random()*(frases.length));
@@ -10,7 +11,4 @@ function geradorMensagem(){
     document.querySelector("#saída").textContent=fraseGerada;
 
     
-
-
 }
-
